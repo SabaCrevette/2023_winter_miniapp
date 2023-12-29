8c836700940cb275423be94b9fac56fee5a118d7
SabaCrevette <sbid.ildr7678@gmail.com>
2023-12-29 15:11:56 +0900

    画像パス修正

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ function App() {
   let images = [
     {
       num: 1, // numberはペアを識別するために使う
-      img: "./img/S.png",
+      img: "./img/S.PNG",
       isMatched: false
     },
     {
       num: 2,
-      img: "./img/T.png",
+      img: "./img/T.PNG",
       isMatched: false
     },
     {
       num: 3,
-      img: "./img/A.png",
+      img: "./img/A.PNG",
       isMatched: false
     },
     {
       num: 4,
-      img: "./img/R.png",
+      img: "./img/R.PNG",
       isMatched: false
     },
     {
       num: 5,
-      img: "./img/T2.png",
+      img: "./img/T2.PNG",
       isMatched: false
     },
   ]
