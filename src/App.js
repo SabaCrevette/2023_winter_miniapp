67e34e361fc3fd216fc06a40161928cb6c31cfce
SabaCrevette <sbid.ildr7678@gmail.com>
2023-12-29 16:38:48 +0900

    add: topページの追加

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import images from './cardImages.js';
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false); // ゲーム開始状態の追加
   const [cards, setCards] = useState([]);
   const [selectedCards, setselectedCards] = useState([]);
   const [tries, setTries] = useState(0);
@@ -39,6 +40,12 @@ function App() {
     }
   }, [cards]);
 
+  // ゲームを開始する関数
+  const startGame = () => {
+    setIsGameStarted(true);
+    shuffleImages(); // ゲーム開始時にカードをシャッフル
+  }
+
   // Twitterで共有するためのURLを生成する関数
   const shareOnTwitter = () => {
     const text = `${tries}手でクリアしました！ https://winter-miniapp-2023-10a6b1683e30.herokuapp.com #新年START神経衰弱`;
@@ -65,35 +72,40 @@ function App() {
 
   return (
     <div className="container">
-      <div className="">
-        <h1>新年START神経衰弱</h1>
-        ルール
-      </div>
-      <div className="cards-container">
-        {cards.map((card)=>(
-          <Card
-            key={card.id}
-            card={card}
-            selectedCards={selectedCards}
-            setselectedCards={setselectedCards}
-          />
-        ))}
-      </div>
-      <div className="tries-count">
-        <h2>現在の手数: {tries}</h2>
-      </div>
-      {/* ゲームクリアメッセージと共有ボタンを条件付きで表示 */}
-      {isGameCleared && (
-        <div className="game-clear-message">
-          <h2>{tries}手でクリアしました！</h2>
-          <button onClick={shareOnTwitter}>Xで共有する</button>
+      {!isGameStarted ? (
+        // ゲームが未開始の場合の表示内容
+        <div className="start-screen">
+          <h1>新年START神経衰弱</h1>
+          <h4>ルール説明</h4>
+          <p>カードをめくって同じ絵を見つけよう！</p>
+          <button onClick={startGame}>START</button>
+        </div>
+      ) : (
+        // ゲームが開始した場合の表示内容
+        <div>
+          <div className="cards-container">
+            {cards.map((card) => (
+              <Card
+                key={card.id}
+                card={card}
+                selectedCards={selectedCards}
+                setselectedCards={setselectedCards}
+              />
+            ))}
+          </div>
+          <div className="tries-count">
+            <h2>現在の手数: {tries}</h2>
+          </div>
+          {/* ゲームクリアメッセージと共有ボタンを条件付きで表示 */}
+          {isGameCleared && (
+            <div className="game-clear-message">
+              <h2>{tries}手でクリアしました！</h2>
+              <button onClick={shareOnTwitter}>で共有する</button>
+            </div>
+          )}
         </div>
       )}
-
-      {/* カードコンテナなど他の要素 */}
-      {/* ... */}
     </div>
-    
   );
 }
 
